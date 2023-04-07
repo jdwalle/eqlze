f2f45b60f8c12dc6b705c6cd2190fa2123a4a4b8
tamaghan <tamaghan@buffalo.edu>
2023-04-07 10:15:15 -0400

    Fix #3 : Hooked up Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          EQLZE
-        </h1>
+        <h1>EQLZE</h1>
         <div className="container">
-        <div className="cube">
-          <div className="sides">
-            <div className="top"></div>
-            <div className="right"></div>
-            <div className="bottom"></div>
-            <div className="left"></div>
-            <div className="front"></div>
-            <div className="back"></div>
+          <div className="cube">
+            <div className="sides">
+              <div className="top"></div>
+              <div className="right"></div>
+              <div className="bottom"></div>
+              <div className="left"></div>
+              <div className="front"></div>
+              <div className="back"></div>
+            </div>
           </div>
+          <div className="text">In Development</div>
         </div>
-        <div className="text">In Development</div>
-      </div>
       </header>
     </div>
   );
