f5b94cd6d61fc42bb9e32743eecfe498544d2a95
Tamaghan Maurya <tamaghanm@gmail.com>
2023-04-20 22:32:07 -0400

    Added Faq entry

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ComingSoon } from './packages/pages/coming-soon/coming-soon';
 import { Home } from './packages/pages/home/home';
+import { Faq } from './packages/pages/faq/faq';
 import Layout from './packages/wrappers/layout/layout';
 
 const App = () => (
@@ -12,6 +13,7 @@ const App = () => (
         <Routes>
           {/* <Route path="/AboutUs" element={<AboutUs />} /> */}
           <Route path="/home" element={<Home />} />
+          <Route path="/faq" element={<Faq />} />
           <Route path="/" element={<ComingSoon />} />
         </Routes>
       </Layout>
---
+++ src/packages/pages/faq/faq.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Faq = () => <div>faq</div>;
