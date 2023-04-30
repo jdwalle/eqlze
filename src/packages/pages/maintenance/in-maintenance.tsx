554bd801a0f67331dca7b2d310333186b3c7014c
Tamaghan Maurya <tamaghanm@gmail.com>
2023-04-30 16:03:13 -0400

    removed coming soon and added maintenance page

---
+++ src/packages/pages/maintenance/in-maintenance.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const InMaintenance = () => <div>sorry</div>;
--- src/packages/wrappers/layout/layout.tsx
+++ src/packages/wrappers/layout/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavBar } from '../../components/navbar/navbar';
 import { ComingSoon } from '../../pages/coming-soon/coming-soon';
+import { InMaintenance } from '../../pages/maintenance/in-maintenance';
 
 import './layout.scss';
 
@@ -8,8 +9,9 @@ import './layout.scss';
 const Layout = (props: any) => {
   const { children, inMaintenance } = props;
   if (inMaintenance) {
-    return <ComingSoon />;
+    return <InMaintenance />;
   }
+
   return (
     <>
       <NavBar />
